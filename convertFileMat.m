cf0509c2d0f34009c082f2b3fd5d5144743517f9
si-yao <siyao.li.szeyiu@gmail.com>
2015-03-18 00:25:34 -0400

    auto

---
+++ convertFileMat.m
@@ -0,0 +1,10 @@
+% this script convert a structured mat to plain mat
+load('RandFileList.mat');
+n =  size(RandFileList, 1);
+for t=1:10
+	for i=1:n
+		FileMat{i, t} = RandFileList(i, t).name;
+	end
+end
+
+save('FileMat.mat', 'FileMat');
